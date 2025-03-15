abae2725ea94bcbfee3626153a27805e2d980281
KaraK <karacsonpeterzsolt@gmail.com>
2025-03-15 11:00:07 +0100

    clenup

--- organise_images/recursive_file_search.py
+++ organise_images/recursive_file_search.py
@@ -3,6 +3,23 @@ import os
 from pathlib import Path
 
 
+"""
+This script recursively finds each file in the provided (and hardcoded) directory trees.
+Also tells what is missing from each others' location. 
+E.g.:
+a_location:
+- alma.txt
+- banan.txt
+
+b_location:
+- banan.txt
+- citrom.txt
+
+present_in_location_a_missing_form_b = [alma.txt]
+present_in_location_b_missing_form_a = [citrom.txt]
+"""
+
+
 def list_all_files(directory):
     file_list = []
     for root, _, files in os.walk(directory):
@@ -15,12 +32,10 @@ def list_all_files(directory):
     return [file.name for file in Path(directory).rglob('*') if file.is_file()]
 
 
-all_files = list_all_files("C:/_move/_Pictures_and_Videos")
-all_files_correct = list_all_files("E:/_move/_Pictures_and_Videos")
+a_location = list_all_files("C:/_move/_Pictures_and_Videos")
+b_location = list_all_files("E:/_move/_Pictures_and_Videos")
 
-# print(set(all_files_correct) - set(all_files))
-in_all_files_but_missing_from_all_files_correct = set(all_files) - set(all_files_correct)
-print(in_all_files_but_missing_from_all_files_correct)
+present_in_location_a_missing_form_b = set(a_location) - set(b_location)
+present_in_location_b_missing_form_a = set(b_location) - set(a_location)
 
-pass
-
+breakpoint()
